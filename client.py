23db9f8705d314c7ce694d6ed4f87b4c0514f287
Rodion <rodbel229@gmail.com>
2024-04-22 16:20:42 +0500

    Chat

--- client.py
+++ client.py
@@ -1,9 +1,40 @@
-import pickle
 import socket
+import sys
 
-HOST = ("localhost", 10000)
+HOST = ("localhost", 10001)
+HEADER_LEN = 10
 
+username = input("Please enter a username").encode()
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(HOST)
-print("Connected to", HOST)
 
+client.connect(HOST)
+client.setblocking(False)
+
+header = f"{len(username):<{HEADER_LEN}}".encode()
+client.send(header+username)
+
+while True:
+    print("Please write a message: ")
+    msg = input().encode()
+
+    if msg:
+        msg_header = f"{len(msg):<{HEADER_LEN}}".encode()
+        client.send(msg_header+msg)
+        print(msg_header+msg)
+    try:
+        while True:
+
+            user_header = client.recv(HEADER_LEN)  # non-blocking
+            if not user_header:
+                sys.exit()
+
+            user_len = int(user_header.decode().strip())
+            username = client.recv(user_len)
+
+            msg_header = client.recv(HEADER_LEN)
+            msg_len = int(msg_header.decode().strip())
+
+            data = client.recv(msg_len).decode()
+            print("New message from: {} - {}".format(username.decode(), data))
+    except IOError as _ex:
+        pass
--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import socket
 import select
 
-HOST = ("localhost", 10000)
+HOST = ("localhost", 10001)
 HEADER_LEN = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,7 +26,7 @@ def receive_msg(client: socket.socket):
 
         return {
             "header": msg_header,
-            "data": client.recv(msg_len).decode("utf-8")
+            "data": client.recv(msg_len)
         }
     except:
         return False
@@ -44,11 +44,13 @@ while True:
             sockets_list.append(client)
             client_list[client] = user
 
-            print("New connection from {} with data {}".format(addr, user["data"]))
+            data = user["data"]
+            print("New connection from {} with data {}".format(addr, data.decode()))
         else:
             client, addr = s.accept()
 
             msg = receive_msg(client)
+            print(msg)
             if msg is False:
                 print("New connection from {} has been interrupted".format(addr))
 
@@ -61,7 +63,7 @@ while True:
 
             for client in client_list:
                 if client is not _socket:
-                    client.send("New message from {} is {}".format(user["data"], msg["data"]).encode())
+                    client.send(user["header"]+user["data"]+msg["header"]+msg["data"])
 
     for _socket in xs:
         sockets_list.remove(_socket)
